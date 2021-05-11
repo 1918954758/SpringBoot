278864643507113f4f87cd3634a26db22e770adc
Administrator <18515489929@163.com>
2021-05-11 11:48:31 +0800

    登录页跳转index页面 使用请求重定向解决表单重复提交的问题

--- boot-01-web-01-admin/src/main/java/com/zichen/admin/controller/IndexController.java
+++ boot-01-web-01-admin/src/main/java/com/zichen/admin/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.zichen.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
@@ -10,4 +11,20 @@ public class IndexController {
     public String login() {
         return "login";
     }
+
+    /*
+     * 主页
+     */
+    @PostMapping("/index")
+    public String index(String username, String password) {
+        // 放置表单重复提交
+        return "redirect:/main.html";
+    }
+
+    // 刷新页面执行该方法，而不是重复提交POST，调用/index请求
+    @GetMapping("/main.html")
+    public String indexPage() {
+        return "index";
+    }
+
 }
