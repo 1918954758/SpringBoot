38b890bc955373fa66e60bf5a1407a2c5ebb77f5
Administrator <18515489929@163.com>
2021-05-12 12:05:18 +0800

    抽取 *-table.html 公共部分到 templates/common/commons.html中

---
+++ boot-01-web-01-admin/src/main/java/com/zichen/admin/controller/TableController.java
@@ -0,0 +1,29 @@
+package com.zichen.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TableController {
+
+    @GetMapping("/basic_table")
+    public String basic_table() {
+        return "table/basic_table";
+    }
+    //templates/table/basic_table.html
+
+    @GetMapping("/dynamic_table")
+    public String dynamic_table() {
+        return "table/dynamic_table";
+    }
+
+    @GetMapping("/responsive_table")
+    public String responsive_table() {
+        return "table/responsive_table";
+    }
+
+    @GetMapping("/editable_table")
+    public String editable_table() {
+        return "table/editable_table";
+    }
+}
