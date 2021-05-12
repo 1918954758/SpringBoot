0c8ffd46cd6b17610918572ebd2bd43f4c2c04a7
Administrator <18515489929@163.com>
2021-05-12 16:10:00 +0800

    - 页面问题修复 - 动态将元素放到页面 - 遍历请求域中的元素，获取值 <tr th:each="dataTest_01:${dataTest_01s}">     <td th:text="${dataTest_01.getDataName()}"></td>     <td>[[${dataTest_01.getDataName()}]]</td> </tr>

---
+++ boot-01-web-01-admin/src/main/java/com/zichen/admin/bean/DataTest_01.java
@@ -0,0 +1,15 @@
+package com.zichen.admin.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class DataTest_01 {
+    private String dataName;
+    private String dataNum;
+}
--- boot-01-web-01-admin/src/main/java/com/zichen/admin/controller/TableController.java
+++ boot-01-web-01-admin/src/main/java/com/zichen/admin/controller/TableController.java
@@ -1,7 +1,12 @@
 package com.zichen.admin.controller;
 
+import com.zichen.admin.bean.DataTest_01;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class TableController {
@@ -13,7 +18,12 @@ public class TableController {
     //templates/table/basic_table.html
 
     @GetMapping("/dynamic_table")
-    public String dynamic_table() {
+    public String dynamic_table(Model model) {
+        List<DataTest_01> dataTest_01s = Arrays.asList(new DataTest_01("aaa", "12"),
+                new DataTest_01("bbb", "13"),
+                new DataTest_01("ccc", "14"),
+                new DataTest_01("ddd", "15"));
+        model.addAttribute("dataTest_01s", dataTest_01s);
         return "table/dynamic_table";
     }
 
