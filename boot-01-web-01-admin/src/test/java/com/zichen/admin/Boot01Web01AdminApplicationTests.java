3e86b6a161a0af0509eb1684740e70bdfddb1cd6
Administrator <18515489929@163.com>
2021-05-24 09:56:15 +0800

    test

---
+++ boot-01-web-01-admin/src/test/java/com/zichen/admin/Boot01Web01AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zichen.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot01Web01AdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
