074db769f9e656c92668e8bbf46290022ebc37a7
Administrator <18515489929@163.com>
2021-06-01 17:14:15 +0800

    operation Redis RedisTemplate & StringRedisTemplate

--- boot-01-web-01-admin/src/test/java/com/zichen/admin/ConnectRedisTest.java
+++ boot-01-web-01-admin/src/test/java/com/zichen/admin/ConnectRedisTest.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+
+import java.nio.charset.StandardCharsets;
 
 /**
  * @name: ConnectRedisTest
@@ -20,14 +22,17 @@ import org.springframework.data.redis.core.ValueOperations;
 public class ConnectRedisTest {
 
     @Autowired
-    StringRedisTemplate redisTemplate;
+    StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    RedisTemplate<Object, Object> redisTemplate;
 
     @Autowired
     RedisConnectionFactory redisConnectionFactory;
 
     @Test
     public void connectRedisTest() {
-        ValueOperations<String, String> stringStringValueOperations = redisTemplate.opsForValue();
+        ValueOperations<String, String> stringStringValueOperations = stringRedisTemplate.opsForValue();
         stringStringValueOperations.set("userName", "zichen");
         String userName = stringStringValueOperations.get("userName");
         log.info(userName);
@@ -38,4 +43,34 @@ public class ConnectRedisTest {
         Class<? extends RedisConnectionFactory> aClass = redisConnectionFactory.getClass();
         log.info(aClass.getName());
     }
+
+
+    @Test
+    public void redisConnectionTest() {
+        ValueOperations<Object, Object> operations = redisTemplate.opsForValue();
+        //ValueOperations<String, String> stringStringValueOperations = stringRedisTemplate.opsForValue();
+        //String key1 = stringStringValueOperations.get("key1");
+        //String key2 = stringStringValueOperations.get("key2");
+        Object key1 = operations.get("key1");
+        Object key2 = operations.get("key2");
+        log.info("key1 【{}】 | key2 【{}】", key1 ,key2);
+
+        /**
+         * 1. 这里使用 RedisTemplate 获取到null：
+         *      - redisTemplate 中存取数据都是字节数组。
+         *      - 当redis中存入的数据是可读形式而非字节数组时，
+         *      - 使用redisTemplate取值的时候会无法获取导出数据，获得的值为null。
+         *      - 可以使用 StringRedisTemplate 试试。
+         */
+    }
+
+    @Test
+    public void redisTemplateTest() {
+        ValueOperations<Object, Object> operations = redisTemplate.opsForValue();
+        operations.set("binary1", "123456");
+
+        Object binary1 = operations.get("binary1");
+        log.info("binary1 = 【{}】", binary1);
+    }
+
 }
